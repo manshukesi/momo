a90bc5759239ab0f910c45b430e832d3cc4b08c0
manshukesi <czqtsmxy@qq.com>
2023-09-14 10:48:34 +0800

    zidong

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global http.sslverify false
 git config --global user.name 'manshukesi'
 git config --global user.email 'czqtsmxy@qq.com'
 git add .
-git commit -m $1
+git commit -m zidong
 git push

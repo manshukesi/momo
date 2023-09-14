ef8a5031057a89c91d75a03f62dbc561e2e56739
manshukesi <czqtsmxy@qq.com>
2023-09-14 10:49:31 +0800

    自动推送

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global http.sslverify false
 git config --global user.name 'manshukesi'
 git config --global user.email 'czqtsmxy@qq.com'
 git add .
-git commit -m zidong
+git commit -m 自动推送
 git push
